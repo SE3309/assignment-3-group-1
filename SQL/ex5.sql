40babe575b427e2e1476d7cffedf31fd29ff1bea
Boris Vasilev <bvasilev@uwo.ca>
2024-11-20 15:25:33 -0500

    Select Statements (only first 2)

---
+++ SQL/ex5.sql
@@ -0,0 +1,31 @@
+-- SELECT STATEMENTS
+
+-- Get user with all their accounts and all their cards
+SELECT "user".name, "user".phone_number, "user".email, "user".date_of_birth,
+       client.student_number, client.status AS client_status,
+       account.balance, account.status AS account_status,
+       account_type.name AS account_type,
+       bank_card.expiry_date AS card_expiry_date, bank_card.card_number, bank_card.status AS card_status, bank_card.daily_limit,
+       card_type.name AS card_type,
+       interest.interest_rate, interest.interest_type
+FROM wob."user"
+JOIN wob.client ON wob.client.user_id = wob."user".user_id
+JOIN wob.account ON wob.account.client_id = wob.client.client_id
+JOIN wob.account_type ON wob.account_type.account_type_id = wob.account.account_type_id
+LEFT JOIN wob.bank_card ON wob.bank_card.account_id = wob.account.account_id
+LEFT JOIN wob.card_type ON wob.card_type.card_type_id = wob.bank_card.card_type_id
+LEFT JOIN wob.interest ON wob.interest.interest_id = wob.card_type.interest_id
+WHERE "user".name = 'Annamaria Hercule Fredenburg';
+
+-- Get staff member, with their role, branch and the branch's address
+SELECT "user".name, "user".phone_number, "user".email, "user".date_of_birth,
+       staff.status AS staff_status,
+       staff_role.name AS staff_role_name, staff_role.base_salary,
+       address.street_number AS branch_street_number, address.street_name AS branch_street_name, address.city AS branch_city, address.province AS branch_province, address.country AS branch_country, address.postal_code AS branch_postal_code
+FROM wob."user"
+JOIN wob.staff ON wob.staff.user_id = wob."user".user_id
+JOIN wob.staff_role ON wob.staff_role.staff_role_id = wob.staff.staff_role_id
+JOIN wob.branch ON wob.branch.branch_id = wob.staff.branch_id
+JOIN wob.address ON wob.address.address_id = wob.branch.address_id
+WHERE "user".name = 'Candice Dolf';
+
